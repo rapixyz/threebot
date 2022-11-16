04bbffbb523ffa90b0a4a1327a53d90f69df65fa
rapixyz <116997555+rapixyz@users.noreply.github.com>
2022-11-16 21:44:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,51 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+
+global.ownername = 'THREE BOT'
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+// Other
+global.owner = ['6285845765943','6285756280433','6285845765943']
+global.premium = ['6285845765943']
+global.packname = 'The Jo Bot'
+global.author = 'WhatsApp Bot'
+global.wlcm = []
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100000
+}
+global.thumb = fs.readFileSync('./lib/three.jpg')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
